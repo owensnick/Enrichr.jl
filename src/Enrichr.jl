e0b7435a062d4ab9e680588bdd69d5fdffc9decf
owensnick <owensnick@gmail.com>
2021-01-15 15:48:40 +0000

    added exports

--- src/Enrichr.jl
+++ src/Enrichr.jl
@@ -6,7 +6,7 @@ using PyCall
 requests = pyimport("requests")
 
 
-export enrichr_genesets,
+export enrichr_genesets, load_genelist, view_genelist, enrichment
 include("enrichr_term_lists.jl")
 include("enrichr_api.jl")
 
